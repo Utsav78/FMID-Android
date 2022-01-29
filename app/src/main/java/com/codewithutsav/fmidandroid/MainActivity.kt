689879c1a6553b9f8bae4f815cb8d0f04448810f
Utsav <utsavbudathoki12@gmail.com>
2022-01-29 20:19:22 +0545

    gallery intent added

--- app/src/main/java/com/codewithutsav/fmidandroid/MainActivity.kt
+++ app/src/main/java/com/codewithutsav/fmidandroid/MainActivity.kt
@@ -1,13 +1,40 @@
 package com.codewithutsav.fmidandroid
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.codewithutsav.fmidandroid.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+
+    companion object{
+        val IMAGE_CODE = 100
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
+    }
+
+    fun openGallery(view: View) {
+        val openGalleryIntent =
+            Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(openGalleryIntent, IMAGE_CODE)
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && data != null && data.data != null){
+            binding.image.setImageURI(data.data)
+        }
     }
 }
