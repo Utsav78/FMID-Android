a2c8dd139103db9775c5be61fdcab49ea5a80511
Utsav <utsavbudathoki12@gmail.com>
2022-02-01 18:58:00 +0545

    added information activity

--- app/src/main/java/com/codewithutsav/fmidandroid/MainActivity.kt
+++ app/src/main/java/com/codewithutsav/fmidandroid/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.codewithutsav.fmidandroid
 
-import android.R.attr
+
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.codewithutsav.fmidandroid.databinding.ActivityMainBinding
@@ -14,10 +14,8 @@ import com.theartofdev.edmodo.cropper.CropImageView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
+    private val TAG = "MainActivity"
 
-    companion object{
-        val IMAGE_CODE = 100
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,9 +26,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun pickImage(view: View) {
-//        val openGalleryIntent =
-//            Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-//        startActivityForResult(openGalleryIntent, IMAGE_CODE)
         CropImage.activity()
             .setGuidelines(CropImageView.Guidelines.ON)
             .start(this)
@@ -40,9 +35,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-//        if (resultCode == RESULT_OK && data != null && data.data != null){
-//            binding.image.setImageURI(data.data)
-//        }
+
         if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
             val result = CropImage.getActivityResult(data)
             if (resultCode == RESULT_OK) {
@@ -50,10 +43,9 @@ class MainActivity : AppCompatActivity() {
                 binding.image.setImageURI(resultUri)
             } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
                 val error = result.error
+                Log.d(TAG, "onActivityResult: ${error.message}")
             }
         }
     }
 
-    fun openCamera(view: View) {
-    }
 }
