6d8974361085bdc5ff7973b8b2fdaa729dd7b914
Utsav <utsavbudathoki12@gmail.com>
2022-02-02 23:24:48 +0545

    nothing

--- app/src/main/java/com/codewithutsav/fmidandroid/InformationActivity.kt
+++ app/src/main/java/com/codewithutsav/fmidandroid/InformationActivity.kt
@@ -2,8 +2,10 @@ package com.codewithutsav.fmidandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codewithutsav.fmidandroid.databinding.ActivityInformationBinding
 
 class InformationActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityInformationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_information)
