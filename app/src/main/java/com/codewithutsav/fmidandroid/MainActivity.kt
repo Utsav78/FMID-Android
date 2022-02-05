2b63a7b1878f6507a277fac0248be38630cd45e6
Utsav <utsavbudathoki12@gmail.com>
2022-02-05 21:15:42 +0545

    tensorflow model added

--- app/src/main/java/com/codewithutsav/fmidandroid/MainActivity.kt
+++ app/src/main/java/com/codewithutsav/fmidandroid/MainActivity.kt
@@ -2,14 +2,18 @@ package com.codewithutsav.fmidandroid
 
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.codewithutsav.fmidandroid.databinding.ActivityMainBinding
+import com.codewithutsav.fmidandroid.ml.Model
 import com.theartofdev.edmodo.cropper.CropImage
 import com.theartofdev.edmodo.cropper.CropImageView
+import org.tensorflow.lite.support.image.TensorImage
 
 
 class MainActivity : AppCompatActivity() {
@@ -40,12 +44,29 @@ class MainActivity : AppCompatActivity() {
             val result = CropImage.getActivityResult(data)
             if (resultCode == RESULT_OK) {
                 val resultUri: Uri = result.uri
+                val bitmap = BitmapFactory.decodeStream(contentResolver.openInputStream(resultUri))
+                outputGenerator(bitmap)
                 binding.image.setImageURI(resultUri)
             } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
                 val error = result.error
                 Log.d(TAG, "onActivityResult: ${error.message}")
             }
         }
+    }
+
+    private fun outputGenerator(bitmap: Bitmap?) {
+        val model = Model.newInstance(this)
+        // converting bitmap into tensor flow image
+        val newBitmap = bitmap?.copy(Bitmap.Config.ARGB_8888, true)
+        val tfImage = TensorImage.fromBitmap(newBitmap).tensorBuffer
+
+        //process the image using trained model and sort it in descending order
+        val outputs = model.process(tfImage)
+            .outputFeature0AsTensorBuffer
+
+
+
+
     }
 
 }
